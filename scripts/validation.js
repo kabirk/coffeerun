16cc83e41a9a8da77b670383d9ba46fe80eea4b5
Your Name <you@example.com>
2017-03-15 18:12:17 -0400

    second commit

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,11 +2,11 @@
   'use strict';
   var App = window.App || {};
 
-  var Validation = {
-    isCompanyEmail: function(email) {
-      return /.@who\,con$/.test(email);
-    }
-  };
+  // var Validation = {
+  //   isCompanyEmail: function(email) {
+  //     return /.@who\,con$/.test(email);
+  //   }
+  // };
 
   App.Validation = Validation;
   window.App = App;
